9fea6b30890fe52cf5be6a8f6a5055f6526c537b
Tokhir Dadaev <zx8754@gmail.com>
2014-01-24 17:15:47 +0000

    Create NCCN.sql

---
+++ NCCN.sql
@@ -0,0 +1,36 @@
+NCCN Field code:
+IF (    IFNULL({Medical\Staging N (Regional Lymph Nodes)},'x',{Medical\Staging N (Regional Lymph Nodes)}) = 'N+'
+	 OR IFNULL({Medical\Staging M (Metastases)},'x',{Medical\Staging M (Metastases)}) = 'M+')
+THEN 'Metastatic'
+ELSE IF (    IFNULL({Medical\Staging T (Stage)},'x',{Medical\Staging T (Stage)}) = 'T3b'
+          OR IFNULL({Medical\Staging T (Stage)},'x',{Medical\Staging T (Stage)}) = 'T3c'
+          OR IFNULL({Medical\Staging T (Stage)},'x',{Medical\Staging T (Stage)}) = 'T4')
+     THEN 'VeryHigh'
+	 ELSE IF (    IFNULL({Medical\Staging T (Stage)},'x',{Medical\Staging T (Stage)}) = 'T3a'
+	           OR (     IFNULL({Query\Practical\GleasonScore},-1,{Query\Practical\GleasonScore}) >= 8
+                    AND IFNULL({Query\Practical\GleasonScore},9999,{Query\Practical\GleasonScore}) <= 10 )
+			   OR IFNULL({Medical\PSA Level at Diagnosis},-1,{Medical\PSA Level at Diagnosis}) > 20)
+	      THEN 'High'
+		  ELSE IF (    IFNULL({Medical\Staging T (Stage)},'x',{Medical\Staging T (Stage)}) = 'T2b'
+		            OR IFNULL({Medical\Staging T (Stage)},'x',{Medical\Staging T (Stage)}) = 'T2c'
+					OR IFNULL({Query\Practical\GleasonScore},-1,{Query\Practical\GleasonScore}) = 7
+					OR (     IFNULL({Medical\PSA Level at Diagnosis},-1,{Medical\PSA Level at Diagnosis}) >= 10
+					     AND IFNULL({Medical\PSA Level at Diagnosis},9999,{Medical\PSA Level at Diagnosis}) <= 20))
+		       THEN 'Intermediate'
+			   ELSE IF (    (    IFNULL({Medical\Staging T (Stage)},'x',{Medical\Staging T (Stage)}) = 'T1'
+			                  OR IFNULL({Medical\Staging T (Stage)},'x',{Medical\Staging T (Stage)}) = 'T1a'
+						      OR IFNULL({Medical\Staging T (Stage)},'x',{Medical\Staging T (Stage)}) = 'T1b'
+						      OR IFNULL({Medical\Staging T (Stage)},'x',{Medical\Staging T (Stage)}) = 'T1c'
+						      OR IFNULL({Medical\Staging T (Stage)},'x',{Medical\Staging T (Stage)}) = 'T2'
+						      OR IFNULL({Medical\Staging T (Stage)},'x',{Medical\Staging T (Stage)}) = 'T2a')
+						  AND (     IFNULL({Query\Practical\GleasonScore},-1,{Query\Practical\GleasonScore}) >= 2
+                                AND IFNULL({Query\Practical\GleasonScore},9999,{Query\Practical\GleasonScore}) <= 6 )
+                          AND (     IFNULL({Medical\PSA Level at Diagnosis},9999,{Medical\PSA Level at Diagnosis}) < 10
+						        AND IFNULL({Medical\PSA Level at Diagnosis},-1,{Medical\PSA Level at Diagnosis}) >= 0))
+			        THEN 'Low'
+					ELSE 'U'
+					ENDIF
+			   ENDIF
+		  ENDIF
+     ENDIF
+ENDIF
